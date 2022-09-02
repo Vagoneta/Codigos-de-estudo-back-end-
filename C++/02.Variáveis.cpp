b988520eb37cc8b3aecbaba838fce7f3a8adba49
Vagoneta <103613467+Vagoneta@users.noreply.github.com>
2022-09-02 15:33:32 -0300

    Add files via upload

---
+++ C++/02.Variáveis.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+	
+	//TIPO NOME;
+	//TIPO NOME = VALOR;
+	
+	int  vidas; // apenas números inteiros
+	char letra; //apenas um caracter ''
+	double decimal; // apenas números reais
+	float decimal2; // Também números reais, porém arredonda o número final
+	bool vivo; //usa valores lógicos: true e false
+	string nome; //apenas textos ""
+
+	return 0;
+	
+}
